3917b04cc9881321c96a3b042c7c602070d22dca
GraceConnelly <grace.connelly.a@gmail.com>
2016-12-20 16:23:18 -0500

    the front end should be able to see this page and it should give them some lots

--- src/main/java/com/theironyard/charlotte/Lot.java
+++ src/main/java/com/theironyard/charlotte/Lot.java
@@ -9,10 +9,10 @@ public class Lot {
     private String id;
     private int capacity;
     private int rate;
-    private ArrayList<String> vehicles;
+    private int vehicles;
     private boolean full;
 
-    public Lot(String id, int capacity, int rate, ArrayList<String> vehicles, boolean full) {
+    public Lot(String id, int capacity, int rate, int vehicles, boolean full) {
         this.id = id;
         this.capacity = capacity;
         this.rate = rate;
@@ -23,4 +23,43 @@ public class Lot {
 
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public int getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(int vehicles) {
+        this.vehicles = vehicles;
+    }
+
+    public boolean isFull() {
+        return full;
+    }
+
+    public void setFull(boolean full) {
+        this.full = full;
+    }
 }
--- src/main/java/com/theironyard/charlotte/Main.java
+++ src/main/java/com/theironyard/charlotte/Main.java
@@ -1,52 +1,51 @@
-//package com.theironyard.charlotte;
-//
-//import jodd.json.JsonParser;
-//import jodd.json.JsonSerializer;
-//import spark.Spark;
-//
-//import java.util.ArrayList;
-//import java.util.Collections;
-//import java.util.List;
-//
-//public class Main {
-//    private static JsonParser parser = new JsonParser();
-//    private static JsonSerializer serializer = new JsonSerializer();
-//
-//    public static void main(String[] args) {
-////        List<Lot> lots = Collections.synchronizedList(new ArrayList<>());
-////
-////        lots.add(new Lot("Clover Street", 25, 5,new ArrayList<String> ()));
-//        Spark.get("/",((request, response) -> {
-//            return serializer.serialize("here is some stuff");
-//        }));
-//
-//        Spark.get("/lots",(request, response)->{
-//            System.out.println("Someone asked for all of the lot info");
-//            return serializer.serialize("some stuff lots");
-//                });
-//        Spark.post("/requestParking", (request, response)-> {
-//            System.out.println("someone wants to park in a lot somewhere, Oh Man!");
-//            return "";
-//        });
-//
-//    }
-//}
 package com.theironyard.charlotte;
 
+import com.sun.xml.internal.bind.v2.runtime.reflect.Lister;
+import jodd.json.JsonParser;
+import jodd.json.JsonSerializer;
 import spark.Spark;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
+    private static JsonParser parser = new JsonParser();
+    private static JsonSerializer serializer = new JsonSerializer();
 
     public static void main(String[] args) {
-//        String port = System.getenv("PORT");
-//
-//        if (port != null) {
-//            Spark.port(Integer.valueOf(port));
-//        }
+        List<Lot> lots = Collections.synchronizedList(new ArrayList<>());
+
+        lots.add(new Lot("Clover St.", 25, 5, 4, false));
+        lots.add(new Lot("Round-A-Bout Ave.", 25, 5, 4, false));
+        lots.add(new Lot("Square Cir.", 25, 5, 4, false));
+        lots.add(new Lot("Ant Ave.", 25, 5, 4, false));
+
+        String port = System.getenv("PORT");
+        if (port != null) {
+            Spark.port(Integer.valueOf(port));
+        }
 
         Spark.before((request, response) -> {
             response.header("Access-Control-Allow-Origin", "*");
         });
+
+        Spark.get("/lots",(request, response)->{
+            System.out.println("Someone asked for all of the lot info");
+            return serializer.serialize(lots);
+                });
+        Spark.post("/requestParking", (request, response)-> {
+            System.out.println("someone wants to park in a lot somewhere, Oh Man!");
+            return "";
+        });
+
+    }
+}
+package com.theironyard.charlotte;
+
+import spark.Spark;
+
+
 
         Spark.get("/", ((request, response) -> "Hello, World!"));
     }
