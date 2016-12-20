ea286f78d4e7073df359a7776a130560bbce0092
GraceConnelly <grace.connelly.a@gmail.com>
2016-12-20 15:59:36 -0500

    Added Maven support to the project

--- src/main/java/com/theironyard/charlotte/Main.java
+++ src/main/java/com/theironyard/charlotte/Main.java
@@ -1,6 +1,5 @@
 package com.theironyard.charlotte;
 
-import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 import jodd.json.JsonParser;
 import jodd.json.JsonSerializer;
 import spark.Spark;
@@ -14,19 +13,18 @@ public class Main {
     private static JsonSerializer serializer = new JsonSerializer();
 
     public static void main(String[] args) {
-        List<Lot> lots = Collections.synchronizedList(new ArrayList<>());
-
-        lots.add(new Lot("Clover Street", 25, 5,new ArrayList<String> ()));
+//        List<Lot> lots = Collections.synchronizedList(new ArrayList<>());
+//
+//        lots.add(new Lot("Clover Street", 25, 5,new ArrayList<String> ()));
 
         Spark.get("/lots",(request, response)->{
             System.out.println("Someone asked for all of the lot info");
-            return serializer.serialize(lots);
+            return serializer.serialize("some stuff lots");
                 });
-        Spark.post("/requestParking", (request, response)->{
+        Spark.post("/requestParking", (request, response)-> {
             System.out.println("someone wants to park in a lot somewhere, Oh Man!");
-
-                }
-        );
+            return "";
+        });
 
     }
 }
