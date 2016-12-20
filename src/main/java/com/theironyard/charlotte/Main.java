1a078a6e9fb6d2f7df2e1d3b184dd517df35e1cc
GraceConnelly <grace.connelly.a@gmail.com>
2016-12-20 16:11:04 -0500

    experimenting without port info

--- src/main/java/com/theironyard/charlotte/Main.java
+++ src/main/java/com/theironyard/charlotte/Main.java
@@ -38,11 +38,11 @@ import spark.Spark;
 public class Main {
 
     public static void main(String[] args) {
-        String port = System.getenv("PORT");
-
-        if (port != null) {
-            Spark.port(Integer.valueOf(port));
-        }
+//        String port = System.getenv("PORT");
+//
+//        if (port != null) {
+//            Spark.port(Integer.valueOf(port));
+//        }
 
         Spark.before((request, response) -> {
             response.header("Access-Control-Allow-Origin", "*");
