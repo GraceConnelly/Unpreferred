6379f31e4223671679782d0d9410b0f05cb37e93
GraceConnelly <grace.connelly.a@gmail.com>
2016-12-20 16:08:51 -0500

    trying to change main to something simple to make site work

--- src/main/java/com/theironyard/charlotte/Main.java
+++ src/main/java/com/theironyard/charlotte/Main.java
@@ -1,33 +1,53 @@
+//package com.theironyard.charlotte;
+//
+//import jodd.json.JsonParser;
+//import jodd.json.JsonSerializer;
+//import spark.Spark;
+//
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+//
+//public class Main {
+//    private static JsonParser parser = new JsonParser();
+//    private static JsonSerializer serializer = new JsonSerializer();
+//
+//    public static void main(String[] args) {
+////        List<Lot> lots = Collections.synchronizedList(new ArrayList<>());
+////
+////        lots.add(new Lot("Clover Street", 25, 5,new ArrayList<String> ()));
+//        Spark.get("/",((request, response) -> {
+//            return serializer.serialize("here is some stuff");
+//        }));
+//
+//        Spark.get("/lots",(request, response)->{
+//            System.out.println("Someone asked for all of the lot info");
+//            return serializer.serialize("some stuff lots");
+//                });
+//        Spark.post("/requestParking", (request, response)-> {
+//            System.out.println("someone wants to park in a lot somewhere, Oh Man!");
+//            return "";
+//        });
+//
+//    }
+//}
 package com.theironyard.charlotte;
 
-import jodd.json.JsonParser;
-import jodd.json.JsonSerializer;
 import spark.Spark;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Main {
-    private static JsonParser parser = new JsonParser();
-    private static JsonSerializer serializer = new JsonSerializer();
 
     public static void main(String[] args) {
-//        List<Lot> lots = Collections.synchronizedList(new ArrayList<>());
-//
-//        lots.add(new Lot("Clover Street", 25, 5,new ArrayList<String> ()));
-        Spark.get("/",((request, response) -> {
-            return serializer.serialize("here is some stuff");
-        }));
+        String port = System.getenv("PORT");
 
-        Spark.get("/lots",(request, response)->{
-            System.out.println("Someone asked for all of the lot info");
-            return serializer.serialize("some stuff lots");
-                });
-        Spark.post("/requestParking", (request, response)-> {
-            System.out.println("someone wants to park in a lot somewhere, Oh Man!");
-            return "";
+        if (port != null) {
+            Spark.port(Integer.valueOf(port));
+        }
+
+        Spark.before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
         });
 
+        Spark.get("/", ((request, response) -> "Hello, World!"));
     }
 }
